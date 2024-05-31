ae28c7018a94d6c8622dbe41d58662178ecd7296
杨志强 <yangzhiqiang@winwin-inc.com>
2024-05-31 14:34:55 +0800

    perf: 默认头像处理

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="header">
-    <div class="logo">
+    <div class="title-box">
       <el-avatar class="user-avator" :size="32" :src="logo" />
-      <span class="title">微信助手</span>
+      <div class="title-info">
+        <span class="title">微信助手</span>
+        <span class="version">wxhelper-3.9.5.81-v14</span>
+      </div>
     </div>
   </div>
 </template>
@@ -15,12 +18,12 @@ import logo from '@/assets/img/logo.png';
 .header {
   position: relative;
   width: 306px;
-  height: 80px;
+  height: 100px;
   padding-top: 10px;
   box-sizing: border-box;
 }
 
-.header .logo {
+.header .title-box {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -31,10 +34,22 @@ import logo from '@/assets/img/logo.png';
     margin-right: 18px;
   }
 
-  .title {
-    font-size: 26px;
-    color: #000;
-    font-weight: bold;
+  .title-info {
+    display: flex;
+    flex-direction: column;
+
+    .title {
+      font-size: 26px;
+      color: #000;
+      font-weight: bold;
+    }
+
+    .version {
+      margin-top: 5px;
+      color: #666;
+      font-size: 14px;
+    }
   }
+
 }
 </style>
