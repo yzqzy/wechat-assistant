94b649de649683ecfd6adaf3dbd31d5d42fadefc
杨志强 <yangzhiqiang@winwin-inc.com>
2024-07-03 18:30:09 +0800

    chore: update node version

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,7 @@ declare module 'vue' {
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElPagination: typeof import('element-plus/es')['ElPagination']
+    ElProgress: typeof import('element-plus/es')['ElProgress']
     ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
